3babd9ac5e52075e758e3331c54a5dd6f0ca2439
PrietoJulii <julieta.prieto@mi.unc.edu.ar>
2022-12-22 16:18:56 -0300

    create base repo

---
+++ com/backend/app/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Verify that the file is not saved with Windows line endings (CRLF).
+# If it is, save it with Unix line endings (LF) and you will be fine.
+# And UTF-8 without BOM (Byte Order Mark) 
+# https://stackoverflow.com/questions/38905135/why-wont-my-docker-entrypoint-sh-execute
+
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres..."
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 0.1
+  done
+    
+  echo "PostgreSQL started"
+fi
+python manage.py makemigrations 
+python manage.py flush --no-input
+python manage.py migrate
+#python manage.py loaddata initial_data.json 
+exec "$@"  # execute the command that was passed to docker run
