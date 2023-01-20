0bb53a738549554ee95a9e5a635e27832edee627
PrietoJulii <julieta.prieto@mi.unc.edu.ar>
2023-01-19 23:09:40 -0300

    feat: user CRUD, login...

--- com/backend/app/api_patrocinio/urls.py
+++ com/backend/app/api_patrocinio/urls.py
@@ -14,10 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from locality.api.router import router_locality
+from user.api.router import router_user
+ 
+from dj_rest_auth.registration.views import RegisterView, ConfirmEmailView, VerifyEmailView, ResendEmailVerificationView
+from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
+from dj_rest_auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('auth/account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('api/', include(router_locality.urls)), 
+    path('auth/verify-email/',VerifyEmailView.as_view(), name='rest_verify_email'),
+    path('auth/confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    re_path(r'^auth/account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(), name='account_confirm_email'),
+    path('auth/resend-email/', ResendEmailVerificationView.as_view(), name='resend-email-verification'),
+    path('auth/password/reset/', PasswordResetView.as_view()),
+    path('auth/password/reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('users/', include(router_user.urls)),
+    path('localities/', include(router_locality.urls, )),
+
 ]
