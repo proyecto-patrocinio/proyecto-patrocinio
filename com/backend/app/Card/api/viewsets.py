34d0f35d68d6694762265f4cbed4973b4fc4b57c
jprieto <jprieto@ascentio.com.ar>
2023-05-04 21:24:28 -0300

    fix: La solicitud retrive de board y panel muestra correctamente los campos.

--- com/backend/app/Card/api/viewsets.py
+++ com/backend/app/Card/api/viewsets.py
@@ -9,15 +9,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()  # all the data in the table
     serializer_class = CardSerializer
-    #permission_classes = [ AllowAny  ] # only authenticated users can access
+    permission_classes = [ IsAuthenticated ]
 
-    permission_classes = [ IsAuthenticated  ]
-
-
-    def list(self, request):
-        # Obtener todas las provincias y serializarlas
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
 
 
--- com/backend/app/Panel/api/viewsets.py
+++ com/backend/app/Panel/api/viewsets.py
@@ -2,23 +2,27 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 from Panel.models import Panel
-from Panel.api.serializers import PanelSerializer
+from Panel.api.serializers import PanelSerializer, PanelFullSerializer
 from rest_framework.response import Response
+from django.db.models import Prefetch
 # Create your views here.
 
 class PanelViewSet(ModelViewSet):
     queryset = Panel.objects.all()
-    serializer_class = PanelSerializer
     permission_classes = [ IsAuthenticated ]
-    # idealmente es IsAuthenticated
-    #permission_classes = [ AllowAny ]
+
+
+    def get_serializer(self, *args, **kwargs): #select json format
+        if self.action == 'list' or self.action == 'create': #by /api/panels/panel/
+            return PanelSerializer(*args, **kwargs)
+        else:
+            return PanelFullSerializer(*args, **kwargs) #by /api/panels/panel/1
     
-    #def get_serializer(self, *args, **kwargs): #select json format
-        #if self.action == 'list' or self.action == 'create': #by /api/locality/
-            #return PanelSerializer(*args, **kwargs)
-        
-    def list(self, request):
-        # Obtener todas las provincias y serializarlas
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self): #select the detail data from the table
+        queryset = super().get_queryset() 
+        if self.action == 'retrieve': #by /api/locality/id
+                queryset = queryset.prefetch_related( 
+                    Prefetch('cards') 
+                )
+        #else is a "list" -> /api/locality/
+        return queryset #return SQL query   
