225b88fd9ba6555b6b404d5004788914a01ca6cd
jprieto <jprieto@ascentio.com.ar>
2023-07-12 00:01:09 -0300

    feat(PAT-26): Add RequestConsultation Entity.

--- com/backend/app/Consultation/api/router.py
+++ com/backend/app/Consultation/api/router.py
@@ -1,6 +1,7 @@
-from Consultation.api.views import ConsultationViewSet
+from Consultation.api.views import ConsultationViewSet, RequestConsultationViewSet
 from rest_framework import routers
 
 router_consultation = routers.DefaultRouter()
 
 router_consultation.register('consultation', viewset=ConsultationViewSet, basename='consultation')
+router_consultation.register('request_consultation', viewset=RequestConsultationViewSet, basename='request_consultation')
--- com/backend/app/Consultation/api/serializers.py
+++ com/backend/app/Consultation/api/serializers.py
@@ -1,8 +1,13 @@
-from Consultation.models import Consultation
+from Consultation.models import Consultation, RequestConsultation
 from rest_framework import serializers
 from Clients.api.serializers import ClientSerializer
 
 class ConsultationSerializer(serializers.ModelSerializer):
   class Meta:
     model = Consultation
+    fields = '__all__'
+
+class RequestConsultationSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = RequestConsultation
     fields = '__all__'
