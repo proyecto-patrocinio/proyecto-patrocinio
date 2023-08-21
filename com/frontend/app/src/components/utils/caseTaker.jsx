8340ec9b769de7a89e5684048e56d26592cc7ac6
jprieto <jprieto@ascentio.com.ar>
2023-08-21 12:58:46 -0300

    docs(PAT-26): Add docstring in  CaseTaker fuctions.

--- com/frontend/app/src/components/utils/caseTaker.jsx
+++ com/frontend/app/src/components/utils/caseTaker.jsx
@@ -1,5 +1,9 @@
 import { List } from "@mui/material";
 
+/**
+ * Fetches consultations that are to be assigned based on REGISTERED status.
+ * @returns {Promise} A promise that resolves to the fetched data or an error.
+ */
 const getConsultationsToAssign = async () => {
     try {
         const url = process.env.REACT_APP_URL_BASE_API_REST_PATROCINIO
@@ -7,18 +11,23 @@ const getConsultationsToAssign = async () => {
                 + "REGISTERED";
         const response = await fetch(url);
         if (response.ok) {
-        const data = await response.json();
-        return data;
+            const data = await response.json();
+            return data;
         } else {
-            console.error('Failed to fetch Consultations:', response.status);
+            console.error('Failed in fetch Consultations:', response.status);
             return null;
         }
     } catch (error) {
-        console.error('Error in fetch Consultations:', error);
+        console.error('Failed in fetch Consultations:', response.status);
+        console.debug(error);
         throw error;
     }
 };
 
+/**
+ * Fetches the list of boards.
+ * @returns {Promise} A promise that resolves to the fetched board list or an error.
+ */
 const getListBoard = async () => {
     try {
         const url = process.env.REACT_APP_URL_BASE_API_REST_PATROCINIO
@@ -28,15 +37,19 @@ const getListBoard = async () => {
             const board_list = await response.json();
             return board_list;
         } else {
-            console.error('Failed to fetch board:', response.status);
             throw new Error('Failed to fetch board');
         }
     } catch (error) {
-        console.error('Error in get list board:', error);
+        console.error('Failed to fetch board:', response.status);
+        console.debug(error);
         throw error;
     }
 }
 
+/**
+ * Fetches the list of request cards.
+ * @returns {Promise} A promise that resolves to the fetched request card list or an error.
+ */
 const getRequestCards = async () => {
     try {
         const url = process.env.REACT_APP_URL_BASE_API_REST_PATROCINIO
@@ -46,11 +59,11 @@ const getRequestCards = async () => {
             const request_card_list = await response.json();
             return request_card_list;
         } else {
-            console.error('Failed to fetch Request Card: ', response.status);
             throw new Error('Failed to fetch Request Card');
         }
     } catch (error) {
-        console.error('Error in get Request Card:', error);
+        console.error('Failed to fetch Request Card: ', response.status);
+        console.debug(error);
         throw error;
     }
 }
