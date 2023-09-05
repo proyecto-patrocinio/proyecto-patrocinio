87c3c0534612b6329fc11bf442849c9362ed8856
jprieto <jprieto@ascentio.com.ar>
2023-09-05 19:31:19 -0300

    refactor(PAT-55): imp tabs.

--- com/backend/app/BoardUSer/models.py
+++ com/backend/app/BoardUSer/models.py
@@ -3,24 +3,26 @@ from django.contrib.auth.models import User
 from Board.models import Board
 
 class BoardUser(models.Model):
-    user  = models.ForeignKey(User,
-                                 on_delete=models.CASCADE,
-                                 verbose_name='User',
-                                 related_name="boardusers",
-                                 null=False,
-                                 )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='User',
+        related_name="boardusers",
+        null=False,
+    )
 
-    board =  models.ForeignKey(Board,
-                                  on_delete=models.CASCADE,
-                                  verbose_name='Board',
-                                  related_name="boardusers",
-                                  null=False,
-                                  )
+    board = models.ForeignKey(
+        Board,
+        on_delete=models.CASCADE,
+        verbose_name='Board',
+        related_name="boardusers",
+        null=False,
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return f'{self.user}_{self.board}'
+        return f'{self.user}/{self.board}'
 
     class Meta:
         unique_together = ('user', 'board')
