e6f3fa8d23d3650158ff7a1dab75a229d35a518f
jprieto <jprieto@ascentio.com.ar>
2023-09-06 21:20:47 -0300

    feat(PAT-59): Create interface to create panel.

---
+++ com/frontend/app/src/components/PanelTemplate.jsx
@@ -0,0 +1,101 @@
+/**
+ * PanelTemplate Component
+ *
+ * This component provides a user interface for creating a panel by entering a title.
+ * It includes an IconButton that opens a dialog when clicked, allowing the user
+ * to input a title and create a panel.
+ *
+ * @component PanelTemplate
+ */
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Button } from '@mui/material';
+
+function PanelTemplate() {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [error, setError] = useState('');
+
+  // Function to open the dialog
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  // Function to close the dialog
+  const handleClose = () => {
+    setOpen(false);
+    setError('');
+  };
+
+  // Function to handle the 'Accept' button click
+  const handleAccept = () => {
+    if (title.trim() === '') {
+      setError('The title cannot be empty.');
+    } else {
+      setOpen(false);
+      setError('');
+      createPanel(title);
+    }
+  };
+
+  // Function to handle title input change
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // Function to create a panel with the provided title
+  const createPanel = (title) => {
+    // TODO: comunicar con el Backend
+    console.info(`A panel was created with the title: ${title}`);
+  };
+
+  return (
+    <div>
+      {/* Tooltip to provide a hint for the IconButton */}
+      <Tooltip title="Create Panel">
+        <IconButton aria-label="delete" color='primary' onClick={handleClickOpen}>
+          <AddCircleIcon />
+        </IconButton>
+      </Tooltip>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Create Panel</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter a title and click 'Accept' to create a panel.
+          </DialogContentText>
+          {/* Input field for entering the title */}
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Title"
+            type="text"
+            fullWidth
+            value={title}
+            onChange={handleChangeTitle}
+            error={!!error}
+            helperText={error}
+          />
+        </DialogContent>
+        {/* Actions in the dialog */}
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleAccept} color="primary">
+            Accept
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default PanelTemplate;
