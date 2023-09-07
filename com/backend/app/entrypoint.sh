a4e5e4c8e21803c6a3a613a524f27f59d2cb8701
jprieto <jprieto@ascentio.com.ar>
2023-09-07 03:27:18 -0300

    fix(PAT-41): Uncoment entrypoint.

--- com/backend/app/entrypoint.sh
+++ com/backend/app/entrypoint.sh
@@ -4,30 +4,30 @@
 # And UTF-8 without BOM (Byte Order Mark) 
 # https://stackoverflow.com/questions/38905135/why-wont-my-docker-entrypoint-sh-execute
 
-# if [ "$DATABASE" = "postgres" ]
-# then
-#   echo "Waiting for postgres..."
-#   while ! nc -z $SQL_HOST $SQL_PORT; do
-#     sleep 0.1
-#   done
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres..."
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
     
-#   echo "PostgreSQL started"
-# fi
-# #migrations
-# python manage.py makemigrations 
-# python manage.py flush --no-input
-# python manage.py migrate
-# python manage.py collectstatic --no-input --clear
+  echo "PostgreSQL started"
+fi
+#migrations
+python manage.py makemigrations 
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py collectstatic --no-input --clear
 
-# #load initial data (the order is important)
-# python manage.py loaddata locality/load_data/nationality.json 
-# python manage.py loaddata locality/load_data/province.json 
-# python manage.py loaddata locality/load_data/locality.json 
+#load initial data (the order is important)
+python manage.py loaddata locality/load_data/nationality.json 
+python manage.py loaddata locality/load_data/province.json 
+python manage.py loaddata locality/load_data/locality.json 
 
-# #run test
-# python manage.py test
+#run test
+python manage.py test
 
-# #create superuser
-# python manage.py createsuperuser --no-input
+#create superuser
+python manage.py createsuperuser --no-input
 
 exec "$@"  # execute the command that was passed to docker run
