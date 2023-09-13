e4b18e1add493142d2381296e20e3cc424cc1740
jprieto <jprieto@ascentio.com.ar>
2023-09-13 00:07:56 -0300

    fix(PAT-65): Show consultation data when card isn't a consultation.

--- com/frontend/app/src/components/ConsutationDisplay.jsx
+++ com/frontend/app/src/components/ConsutationDisplay.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 import { Dialog, DialogTitle, DialogContent, Button} from '@mui/material';
 import TableContainer from '@mui/material/TableContainer';
 import Table from '@mui/material/Table';
@@ -6,9 +7,33 @@ import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import {formatTimestamp} from './utils/format.jsx';
+import {getConsultation} from './utils/caseTaker.jsx';
 
 
-const ConsutationDisplay = ({ consultation, open, onClose }) => {
+const ConsutationDisplay = ({consultation, open, onClose }) => {
+    const [consultationData, setConsultation] = useState(consultation)
+
+	useEffect(() => {
+		const fetchConsultancy = async () => {
+		try {
+            if (consultation.client) {
+                // Get Client information
+                //TODO: get clietn data
+            } else { // Don't have Data in Consultation
+                // Get Data Consultation from Backend
+                const consultationResponse = await getConsultation(consultation.consultation)
+                //TODO: get clietn data
+                setConsultation(consultationResponse)
+            }
+
+		} catch (error) {
+            console.error("Failed to fetch Consultation in Card.");
+			console.debug(error);
+		}
+  };
+  
+  fetchConsultancy();
+  }, []);
 
     const handleClose = () => {
         onClose();
@@ -28,27 +53,27 @@ const ConsutationDisplay = ({ consultation, open, onClose }) => {
             <TableBody>
                 <TableRow>
                 <TableCell>Tag:</TableCell>
-                <TableCell>{consultation.tag}</TableCell>
+                <TableCell>{consultationData.tag}</TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>Client:</TableCell>
-                <TableCell>{consultation.client}</TableCell>
+                <TableCell>{consultationData.client}</TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>Opponent:</TableCell>
-                <TableCell>{consultation.opponent}</TableCell>
+                <TableCell>{consultationData.opponent}</TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>State:</TableCell>
-                <TableCell>{consultation.state}</TableCell>
+                <TableCell>{consultationData.state}</TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>Description:</TableCell>
-                <TableCell>{consultation.description}</TableCell>
+                <TableCell>{consultationData.description}</TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>Creation time stamp:</TableCell>
-                <TableCell>{formatTimestamp(consultation.time_stamp)}</TableCell>
+                <TableCell>{formatTimestamp(consultationData.time_stamp)}</TableCell>
                 </TableRow>
                 {/* Agrega más filas de tabla según los datos de tu consulta */}
             </TableBody>
