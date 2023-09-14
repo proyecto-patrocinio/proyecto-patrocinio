28247f0905982aa1c4ede42344013e4f925b02c9
jprieto <jprieto@ascentio.com.ar>
2023-09-14 03:51:01 -0300

    fix(PAT-65): Update Choices in Client. Convert choices in enlish and imp names.

--- com/backend/app/Clients/models.py
+++ com/backend/app/Clients/models.py
@@ -6,17 +6,17 @@ class Client(models.Model):
     id = models.AutoField( primary_key=True)
     postal = models.IntegerField(verbose_name='Código postal')
     address = models.TextField(max_length=100,verbose_name='Dirección')
-    marital_status = models.CharField(choices=marital_status, max_length=100, verbose_name='Estado civil',)
-    housing_type = models.CharField( choices=housing_type ,max_length=200, verbose_name='Tipo de Vivienda')  
-    studies = models.CharField(choices=studies, max_length=200, verbose_name='nivel de educacion alcanzado')  
+    marital_status = models.CharField(choices=marital_status, max_length=10, verbose_name='Estado civil',)
+    housing_type = models.CharField( choices=housing_type ,max_length=20, verbose_name='Tipo de Vivienda')  
+    studies = models.CharField(choices=studies, max_length=30, verbose_name='nivel de educacion alcanzado')  
     locality = models.ForeignKey(Locality, on_delete=models.CASCADE)  
     email = models.EmailField( unique=True,  verbose_name='correo electronico')
-    id_type= models.CharField(choices=id_type,max_length=1 , verbose_name='Tipo de Documento')  
+    id_type= models.CharField(choices=id_type,max_length=10, verbose_name='Tipo de Documento')  
     id_number = models.IntegerField( unique=True, verbose_name='Número de Documento')  
     first_name = models.CharField(max_length=150, verbose_name='Nombre')
     last_name = models.CharField(max_length=150, verbose_name='Apellido')
     birth_date = models.DateField(verbose_name='Fecha de Nacimiento',)
-    sex = models.CharField(choices=sex, max_length=1, default='F', verbose_name='Sexo')
+    sex = models.CharField(choices=sex, max_length=6, default='FEMALE', verbose_name='Sexo')
 
     def __str__(self) -> str:
         return f'{self.last_name} {self.first_name}'
