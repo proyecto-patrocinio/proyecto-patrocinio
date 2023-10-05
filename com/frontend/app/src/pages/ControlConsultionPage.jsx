7b77e54c18b4c83a6b345943cae9d6327822ec14
jprieto <jprieto@ascentio.com.ar>
2023-10-05 01:13:41 -0300

    feat(PAT-72): Add control panel consultations page.

--- com/frontend/app/src/pages/ControlConsultionPage.jsx
+++ com/frontend/app/src/pages/ControlConsultionPage.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Dashboard from '../containers/Dashboard';
+import ConsultationDataTable from '../components/ConsultionDataTable';
+import { getConsultationList } from '../utils/caseTaker';
 
 const theme = createTheme();
-export default function ControlPanelConsultation( ) {
+
+
+export default function ControlPanelConsultation() {
+    const [consultationList, setConsultationList] = useState([])
+
+    useEffect(() =>{
+        const fetchClients = async() => {
+            const clientList = await getConsultationList();
+            setConsultationList(clientList);
+        }
+        fetchClients();
+    }, [])
 
     return (
         <ThemeProvider theme={theme}>
-            <Dashboard title={"CONSULTANCY"}>
-            Control Consultation Page
+            <Dashboard title={"Control Panel - Consultations"}>
+                <ConsultationDataTable data={consultationList} />
             </Dashboard>
         </ThemeProvider>
     );
