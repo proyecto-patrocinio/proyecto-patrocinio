ca136671df39091333c5869c04e4d8a51836f744
jprieto <jprieto@ascentio.com.ar>
2023-10-07 15:12:28 -0300

    feat(PAT-73): Add fuctions to process delete, create, and update client with the api.

--- com/frontend/app/src/utils/client.jsx
+++ com/frontend/app/src/utils/client.jsx
@@ -17,8 +17,8 @@ export async function getDataClient(clientID) {
         + clientID;
       const response = await fetch(url);
       if (response.ok) {
-        const board = await response.json();
-        return board;
+        const client = await response.json();
+        return client;
       } else {
         console.error('Failed to fetch client:', response.status);
         throw new Error('Failed to fetch client');
@@ -27,7 +27,7 @@ export async function getDataClient(clientID) {
       console.error('Error while try to get client:', error);
       throw error;
     }
-  }
+};
 
 
 /**
@@ -42,8 +42,8 @@ export async function getClientList() {
       + process.env.REACT_APP_PATH_CLIENTS;
     const response = await fetch(url);
     if (response.ok) {
-      const board = await response.json();
-      return board;
+      const client = await response.json();
+      return client;
     } else {
       console.error('Failed to fetch list of client:', response.status);
       throw new Error('Failed to fetch list of client');
@@ -52,4 +52,110 @@ export async function getClientList() {
     console.error('Error while try to get list of client:', error);
     throw error;
   }
-}
+};
+
+
+/**
+ * Creates a new client by sending a POST request to the API.
+ *
+ * @param {object} clientData - The data for the new client.
+ * @returns {Promise<object>} A promise that resolves to the created client.
+ * @throws {Error} If the creation fails, an error is thrown.
+ */
+export async function createClient(clientData) {
+  try {
+    const url = process.env.REACT_APP_URL_BASE_API_REST_PATROCINIO
+      + process.env.REACT_APP_PATH_CLIENTS;
+    const response = await fetch(
+      url,
+      {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(clientData)
+      });
+    if (response.ok) {
+      const client = await response.json();
+      return client;
+    } else {
+      const mns = 'Failed to create a new client.';
+      console.error(mns, " Status: ", response.status);
+      throw new Error(mns);
+    };
+  } catch (error) {
+    console.error('Error while try to create a client:', error);
+    throw error;
+  };
+};
+
+
+/**
+ * Deletes a client by sending a DELETE request to the API.
+ *
+ * @param {string} clientID - The ID of the client to be deleted.
+ * @returns {Promise<object>} A promise that response.
+ * @throws {Error} If the deletion fails, an error is thrown.
+ */
+export async function deleteClient(clientID) {
+  try {
+    const url = process.env.REACT_APP_URL_BASE_API_REST_PATROCINIO
+      + process.env.REACT_APP_PATH_CLIENTS
+      + clientID + '/';
+    const response = await fetch(
+      url,
+      {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (response.ok) {
+      return response;
+    } else {
+      const mns = 'Failed to delete client.'
+      console.error(mns, " Status: ", response.status);
+      throw new Error(mns);
+    }
+  } catch (error) {
+    console.error('Error while try to delete a client: ', error);
+    throw error;
+  }
+};
+
+
+/**
+ * Updates an existing client by sending a PUT request to the API.
+ *
+ * @param {object} clientData - The updated data for the client.
+ * @returns {Promise<object>} A promise that resolves to the updated client.
+ * @throws {Error} If the update fails, an error is thrown.
+ */
+export async function updateClient(clientData) {
+  try {
+    const url = process.env.REACT_APP_URL_BASE_API_REST_PATROCINIO
+      + process.env.REACT_APP_PATH_CLIENTS
+      + clientData.id
+      + "/";
+    const response = await fetch(
+      url,
+      {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(clientData)
+      });
+    if (response.ok) {
+      const client = await response.json();
+      return client;
+    } else {
+      const mns = 'Failed to update a new client.';
+      console.error(mns, " Status: ", response.status);
+      throw new Error(mns);
+    };
+  } catch (error) {
+    console.error('Error while try to update a client: ', error);
+    throw error;
+  };
+};
