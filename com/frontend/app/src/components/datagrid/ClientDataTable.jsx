7ca72d6975b9b4faf351daa17782ddc06fd47577
jprieto <jprieto@ascentio.com.ar>
2023-10-15 18:02:03 -0300

    docs(PAT-91): Add docstring.

--- com/frontend/app/src/components/datagrid/ClientDataTable.jsx
+++ com/frontend/app/src/components/datagrid/ClientDataTable.jsx
@@ -71,6 +71,17 @@ function ClientDataTable({ data }) {
     return params.colDef.editable;
   };
 
+  /**
+   * A cell component for the Material-UI (MUI) DataGrid table used in renderEditCell.
+   * This component is designed to work with fields related to locality, province, and nation.
+   *
+   * @param {Object} props - The component's properties.
+   * @param {Array} props.optionsNameID - The list of options with objects containing 'name' and 'id' properties.
+   * @param {Object} props.model - The data model for the cell.
+   * @param {function} props.handleChange - The function called when the selected value in the Autocomplete changes.
+   *
+   * @returns {JSX.Element} - The rendered AutocompleteCell component.
+   */
     function AutocompleteCell(props) {
       const nameToIdMap = {};
       props.optionsNameID?.forEach((item) => {
