56439363939998d8c5751e38526202cc6295d728
jprieto <jprieto@ascentio.com.ar>
2023-10-29 05:14:05 -0300

    fix(PAT-35): get terms and conditions.

--- com/backend/app/terms_and_policies/viewsets.py
+++ com/backend/app/terms_and_policies/viewsets.py
@@ -1,12 +1,14 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import AllowAny
 from django.http import HttpResponse
 import os
 
 TERMS_FILE_NAME = 'terms_and_policies/terms_and_policies.md'
 
 @api_view(http_method_names=['GET'])
+@permission_classes([AllowAny])
+@authentication_classes([])
 def get_file_contentView(request):
-    permission_classes = (AllowAny,)
     try:
         file_path = os.path.join(os.curdir, TERMS_FILE_NAME)
         with open(file_path, 'r') as file:
@@ -16,10 +18,10 @@ def get_file_contentView(request):
     except FileNotFoundError:
         return HttpResponse('The terms and policies file was not found.', status=404)
 
-
 @api_view(http_method_names=['GET'])
+@permission_classes([AllowAny])
+@authentication_classes([])
 def download_file_contentView(request):
-    permission_classes = (AllowAny,)
     try:
         file_path = os.path.join(os.curdir, TERMS_FILE_NAME)
         with open(file_path, 'r') as file:
