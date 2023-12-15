ec5def836923443cc6e35c03d1ebd4a562834e66
jprieto <jprieto@ascentio.com.ar>
2023-12-15 01:59:44 -0300

    refactor(PAT-127): Fix pipeliene for flake8 .

--- com/backend/app/api_patrocinio/settings.py
+++ com/backend/app/api_patrocinio/settings.py
@@ -206,7 +206,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_PORT = 587
 # Protection against brute-force attacks.
 ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
-ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT  = 300
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
 
 # CORS- Cross-Origin
 # https://pypi.org/project/django-cors-headers/
